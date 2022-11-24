692f5880ae0e16646ccbd00c1f553803d32824df
nad388 <nad388@yandex.ru>
2022-11-24 14:13:34 +0600

    added axios, api, castom hooks, loader

--- src/App.js
+++ src/App.js
@@ -1,39 +1,37 @@
-import React, { useMemo, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import PostService from './API/PostService';
 import PostFilter from './components/PostFilter';
 import PostForm from './components/PostForm';
 import PostList from './components/PostList';
 import MyButton from './components/UI/button/MyButton';
+import Loader from './components/UI/loader/Loader';
 import MyModal from './components/UI/myModal/MyModal';
+import { usePosts } from './hooks/usePost';
 import './styles/App.css';
 
 function App() {
-  const [posts, setPosts] = useState([
-    { id: 1, title: 'JavaScript', body: 'Description1' },
-    { id: 2, title: 'TypeScript', body: 'Description2' },
-    { id: 3, title: 'CoffeeScript', body: 'Description3' },
-  ]);
+  const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({ sort: '', query: '' });
   const [modal, setModal] = useState(false);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  const [isPostsLoading, setIsPostsLoading] = useState(false);
 
-  const sortedPosts = useMemo(() => {
-    if (filter.sort) {
-      return [...posts].sort((a, b) =>
-        a[filter.sort].localeCompare(b[filter.sort])
-      );
-    }
-    return posts;
-  }, [filter.sort, posts]);
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter((post) =>
-      post.title.toLowerCase().includes(filter.query)
-    );
-  }, [filter.query, sortedPosts]);
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
     setModal(false);
   };
+
+  async function fetchPosts() {
+    setIsPostsLoading(true);
+    const posts = await PostService.getAll();
+    setPosts(posts);
+    setIsPostsLoading(false);
+  }
+
   // post из дочернего элемента
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
@@ -50,11 +48,23 @@ function App() {
 
       <hr style={{ margin: '15px 0', color: 'rgba(22, 18, 226, 0.699)' }} />
       <PostFilter filter={filter} setFilter={setFilter} />
-      <PostList
-        remove={removePost}
-        posts={sortedAndSearchedPosts}
-        title='Post list'
-      />
+      {isPostsLoading ? (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            marginTop: '50px',
+          }}
+        >
+          <Loader />
+        </div>
+      ) : (
+        <PostList
+          remove={removePost}
+          posts={sortedAndSearchedPosts}
+          title='Post list'
+        />
+      )}
     </div>
   );
 }
