839fadafc9e89ab4406822af7a7bb7132f30f4ba
nad388 <nad388@yandex.ru>
2022-11-25 12:48:27 +0600

    added post page

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import About from '../pages/About';
 import Error from '../pages/Error';
+import PostIdPage from '../pages/PostIdPage';
 import Posts from '../pages/Posts';
 const AppRouter = () => {
   return (
     <Routes>
       <Route path='/about' element={<About />} />
-      <Route path='/posts' element={<Posts />} />
+      <Route exact path='/posts' element={<Posts />} />
+      <Route path='/' element={<Posts />} />
+      <Route exact path='/posts/:id' element={<PostIdPage />} />
       {/* <Route exact path='/' element={<Navigate to='/posts' />} /> */}
       <Route path='*' element={<Error />} />
     </Routes>
